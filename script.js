6561f35b3ac688f60a4881365278969bef2597c8
rslgp <rslgp@cin.ufpe.br>
2023-09-11 07:13:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,51 @@ function onMapClick(e) {
 
 map.on('click', onMapClick);
 
+
+function createProductCard(product) {
+  const card = document.createElement("div");
+  card.classList.add("product-card");
+
+  const productName = document.createElement("h2");
+  productName.textContent = product["nome-mercado"];
+
+  const price = document.createElement("p");
+  price.textContent = `Price: $${product["preco"].toFixed(2)}`;
+
+  const weight = document.createElement("p");
+  weight.textContent = `Weight: ${product["peso"]}g`;
+
+  const pricePerGram = document.createElement("p");
+  pricePerGram.textContent = `Price per Gram: $${product["preco-por-g"].toFixed(5)}`;
+
+  const productCategory = document.createElement("p");
+  productCategory.textContent = `Category: ${product["categoria"]}`;
+
+  const marketId = document.createElement("p");
+  marketId.textContent = `Market ID: ${product["marketid"]}`;
+
+  card.appendChild(productName);
+  card.appendChild(price);
+  card.appendChild(weight);
+  card.appendChild(pricePerGram);
+  card.appendChild(productCategory);
+  card.appendChild(marketId);
+
+  return card;
+}
+
+function displayProductCards(products) {
+  const productCardsContainer = document.getElementById("productCards");
+  productCardsContainer.innerHTML = ""; // Clear previous cards
+
+  products.forEach((product) => {
+    const card = createProductCard(product);
+    productCardsContainer.appendChild(card);
+  });
+}
+
+// Display product cards when the page loads
+
 // Function to search for a product
 function searchProduct() {
     const productName = document.getElementById('productName').value;
@@ -33,8 +78,9 @@ function searchProduct() {
         .then(response => response.json())
         .then(data => {
             // Display the result on the page
-            const resultDiv = document.getElementById('result');
-            resultDiv.innerHTML = JSON.stringify(data, null, 2);
+            //const resultDiv = document.getElementById('result');
+            //resultDiv.innerHTML = JSON.stringify(data, null, 2);
+            displayProductCards(data);
         })
         .catch(error => {
             console.error('Error:', error);
